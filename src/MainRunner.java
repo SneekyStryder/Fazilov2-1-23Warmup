b26ce1026c08319497c64b0272adabc30091227f
Imran Fazilov <ifazilov3584@bths.edu>
2023-02-01 11:23:43 -0500

    done

--- src/MainRunner.java
+++ src/MainRunner.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class MainRunner {
+import java.util.*;
+
+public class MainRunner {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Homework");
+        list.add("Practice");
+        list.add("Sleep");
+        list.add("Live");
+        System.out.println(list);
+        String initial = list.set(0, list.get(3));
+        list.set(3, initial);
+        System.out.println(list.get(0));
+        System.out.println(list.get(3));
+        list.set(0, "HELLO!");
+        System.out.println(list);
+    }
 }
